14d84c33c3efd39c6392180e00bd3ffe5e51fea7
Walt <walt@waltnixon.com>
2013-05-23 19:03:48 -0500

    d3Log Scale Added, Updated buttons

--- js/decline.js
+++ js/decline.js
@@ -20,7 +20,7 @@ var wellDefault = {
 	"bFactorGas": "1.0",
 	"bFactorWater": "1.0",
 	"initGasDecline": "90",
-	"initOilDecline": "80",
+	"initOilDecline": "70",
 	"initWaterDecline": "90",
 	"timeStart": "",
 	"econLife": "10",
@@ -28,17 +28,17 @@ var wellDefault = {
 	"loeCost": "5000.0",
 	"production": [
 		{
-			"oil": "",
-			"gas": "",
-			"water": "",
-			"timeSinceStart":""
+			"oil": [],
+			"gas": [],
+			"water": [],
+			"month": []
 		}],
 	"revenue": [
 		{
-			"oil": "",
-			"gas": "",
-			"water": "",
-			"timeSinceStart":""
+			"oil": [],
+			"gas": [],
+			"water": [],
+			"month":[]
 		}
 	]
 }
@@ -59,18 +59,18 @@ var wellUser = {
 	"loeCost": "5000.0",
 	"production": [
 		{
-			"oil": "",
-			"gas": "",
-			"water": "",
-			"timeSinceStart":""
+			"oil": [],
+			"gas": [],
+			"water": [],
+			"month":[]
 
 		}],
 	"revenue": [
 		{
-			"oil": "",
-			"gas": "",
-			"water": "",
-			"timeSinceStart":""
+			"oil": [],
+			"gas": [],
+			"water": [],
+			"month":[]
 		}
 	]
 }
@@ -82,32 +82,12 @@ var wellUser = {
 function hyperDecline (initProduction, initDecline, bFactor, timeFromStart) {
 	var q = 0;
 	var qi = initProduction;
-	var Di = initDecline;
+	var Di = initDecline/1200.0;
 	var b = bFactor;
 	var tempStep = (1+b*Di*timeFromStart);
 	var q = Math.pow((qi / tempStep), (1.0/b));
 	/*console.log("q set = " + q);*/
 	return q;
-}
-
-function processOil (time) {
-	// console.log("In processOil function");
-	var ip, Di, b;
-	if (global.useDefault == "true"){
-		console.log("use default == true");
-  		global.econLife = wellDefault.econLife;
-  		ip = wellDefault.initOilProduction;
-  		Di = wellDefault.initOilDecline/100.0;
-  		b = wellDefault.bFactorOil;
-  	}
-  	else {
-  		console.log("use default == false");
-  		global.econLife = wellUser.econLife;
-  		ip = wellUser.initOilProduction;
-  		Di = wellUser.initOilDecline/100.0;
-  		b = wellUser.bFactorOil;
-  	}
-  	return [time, hyperDecline(ip, Di, b, time)];
 }
 
 
@@ -205,51 +185,65 @@ $('#afeCost').change(function() {
 $('#useDefault').click(function() {
 	console.log("useDefault click event handler fired");
 	global.useDefault = "true";
-	global.econLife = wellDefault.econLife;
 	console.log("global.useDefault set = " + global.useDefault);
-	console.log("global.econLife set = " + global.econLife);
-});
 
+  	for (var t=0; t<parseFloat(wellDefault.econLife)*12; t++){
+  		// console.log("in for loop");
+  		var oilProd = hyperDecline(wellDefault.initOilProduction,
+  			wellDefault.initOilDecline,
+  			wellDefault.bFactorOil,
+  			t);
+  		wellDefault.production[0].oil.push(oilProd);
+  		wellDefault.production[0].month.push(t);
+  	}
+  console.log("wellDefault Production = " + wellDefault.production);
+  drawGraph(wellDefault.production);
+  return false;
+});
 
 
 $('#processData').click(function() {
 	console.log("processData click handler fired");
-	console.log("global.econLife" + global.econLife);
-	console.log("parseFloat(global.econLife) " + parseFloat(global.econLife));
-  	for (var t=1; t<=parseFloat(global.econLife)*12; t++){
+
+  	for (var t=0; t<parseFloat(wellUser.econLife)*12; t++){
   		// console.log("in for loop");
-  		oilProd = processOil(t);
-  		global.prodOil.push(oilProd);
+  		var oilProd = hyperDecline(wellUser.initOilProduction,
+  			wellUser.initOilDecline,
+  			wellUser.bFactorOil,
+  			t);
+  		wellUser.production[0].oil.push(oilProd);
+  		wellUser.production[0].month.push(t);
   	}
-  console.log(global.prodOil);
-  drawGraph();
+  console.log("wellUser Production = " + wellUser.production);
+  drawGraph(wellUser.production);
   return false;
 });
 
 /*D3 Graph Area 
 =========================================================
 */
-function getMaxMonth(life) {
-	return parseFloat(life)*12
-}
 
-
-function drawGraph () { 
+function drawGraph (production) { 
 	var margin = {top: 20, right:20, bottom:20, left:50},
-		height = 250 - margin.left - margin.right,
-		width = 400 - margin.top - margin.bottom;
+		height = 300 - margin.left - margin.right,
+		width = 475 - margin.top - margin.bottom;
+	
+	var data = [];
+	for (var i=0; i < d3.max(production[0].month); i++){
+		data.push([production[0].month[i], production[0].oil[i]]);
+	}
+	console.log(data);
+	console.log(d3.max(production[0].oil));
+	console.log(d3.max(production[0].month));
 
 	var parseDate = d3.time.format("%Y%m%d").parse;
 
 	var x = d3.scale.linear().
-		domain([0, getMaxMonth(global.econLife)]). // your data minimum and maximum
+		domain([0, d3.max(production[0].month)]). // your data minimum and maximum
 		range([0, width]); // the pixels to map to, e.g., the width of the diagram.
 
-
-	//console.log(d3.max(global.oilProd, function(d) { return d; }));
-
-	var y = d3.scale.linear()
-		.domain([0, 120])
+	var y = d3.scale.log()
+		.domain([1, d3.max(production[0].oil)*10.0])
 		.range([height, 0]);
 
 	var xAxis = d3.svg.axis()
@@ -258,11 +252,13 @@ function drawGraph () {
 
     var yAxis = d3.svg.axis()
         .scale(y)
+        .tickValues([1,10,100,1000,10000])
+        .tickFormat(d3.format(",.0f"))
         .orient("left");
 
 	var svg = d3.select("#graph")
 	    .append("svg")
-	    .data(global.econLife)
+	    .data(production)
 	    .attr("width", width + margin.left + margin.right)
 	    .attr("height", height + margin.top + margin.bottom)
 	    .append("g")
@@ -282,7 +278,7 @@ function drawGraph () {
 		.y(function(d) { return y(d[1]); });
 
 	svg.append("svg:path")
-		.attr("d", line(global.prodOil))
+		.attr("d", line(data))
 		.style("stroke", function() { return "#000000";})
 		.style("fill", "none")
 		.style("stroke-width", "2.5");
@@ -290,7 +286,7 @@ function drawGraph () {
 	var dataCirclesGroup = svg.append('svg:g');
  
 	var circles = dataCirclesGroup.selectAll('.data-point')
-		.data(global.prodOil);
+		.data(data);
 
 	circles
 			.enter()
